7a2762dd93a5493e503cce1e14de8c8b117408b1
Adam Connolly <adam.connolly66@gmail.com>
2020-05-08 10:15:34 -0500

    added List to user. Contact model.

---
+++ Flight Tracker/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flight_Tracker.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        [ForeignKey ("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+    }
+}
