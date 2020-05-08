6f446159565670beecc07173df0b3edd3a845f56
Jonathan Friedl <jfreetz@yahoo.com>
2020-05-08 13:54:40 -0500

    added identity user role

---
+++ Flight Tracker/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Flight_Tracker.Data;
+using Flight_Tracker.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Flight_Tracker.Areas.Identity.IdentityHostingStartup))]
+namespace Flight_Tracker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
